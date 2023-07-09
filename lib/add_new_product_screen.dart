e33cddb835050ff30f04c4727583a6e22f87cf60
syfulsharif <sharif.syful@gmail.com>
2023-07-10 00:09:31 +0600

    Add Product TextFormFields Added

--- lib/add_new_product_screen.dart
+++ lib/add_new_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:live_class1_module9/style.dart';
 
 class AddNewProductScreen extends StatefulWidget {
   const AddNewProductScreen({super.key});
@@ -14,8 +15,44 @@ class _AddNewProductScreenState extends State<AddNewProductScreen> {
       appBar: AppBar(
         title: const Text('Add Product'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: appInputDecoration('Product Name'),
+            ),
+            TextFormField(
+              decoration: appInputDecoration('Product Code'),
+            ),
+            TextFormField(
+              decoration: appInputDecoration('Price'),
+            ),
+            TextFormField(
+              decoration: appInputDecoration('Quantity'),
+            ),
+            TextFormField(
+              decoration: appInputDecoration('Total Price'),
+            ),
+            TextFormField(
+              decoration: appInputDecoration('Image'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(vertical: 12)
+                ),
+                onPressed: () {},
+                child: const Text('Add'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+InputDecoration appInputDecoration(hint){
+  return InputDecoration(
+    hintText: hint,
+  );
+}
